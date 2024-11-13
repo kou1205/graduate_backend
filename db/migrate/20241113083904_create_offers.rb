7ddab5ce32047f016e99e7d88ed0d571dfeff1b5
kou1205 <1375846@gmail.com>
2024-11-13 17:54:25 +0900

    Revert "Feature/company"

--- db/migrate/20241113083904_create_offers.rb
+++
@@ -1,9 +0,0 @@
-class CreateOffers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :offers do |t|
-      t.references :company, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
